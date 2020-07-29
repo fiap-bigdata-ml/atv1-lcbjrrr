8cce37d906fbdb5ed1ebe6b14ca2c7afd2b1ec30
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2020-07-29 12:19:36 -0300

    ok

--- py1.py
+++ py1.py
@@ -2,7 +2,7 @@ def nota_final(ap1, ap2, ap3):
     return ap1*0.4 + ap2*0.4 + ap3*0.2
 
 def imc(peso,altura):
-    return peso/(altura*altura)
+    return 0.01
 
 def FtoC(f):
   c = (5 * (f-32) / 9)
